023c652657159df551250af15b3ef88253a488f2
CristoSparda <cristo.n6-@hotmail.com>
2021-05-14 16:45:13 -0500

    Añadido de los metodos faltantes para el StreetServiceImpl

--- src/test/java/org/uresti/pozarreal/service/impl/StreetServiceImplTests.java
+++ src/test/java/org/uresti/pozarreal/service/impl/StreetServiceImplTests.java
@@ -1,18 +1,19 @@
 package org.uresti.pozarreal.service.impl;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.uresti.pozarreal.dto.StreetInfo;
+import org.uresti.pozarreal.model.House;
+import org.uresti.pozarreal.model.Representative;
 import org.uresti.pozarreal.model.Street;
 import org.uresti.pozarreal.repository.HousesRepository;
 import org.uresti.pozarreal.repository.RepresentativeRepository;
 import org.uresti.pozarreal.repository.StreetRepository;
 
 import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StreetServiceImplTests {
 
@@ -32,7 +33,7 @@ public class StreetServiceImplTests {
         List<Street> streets = streetService.getStreets();
 
         // Then:
-        Assertions.assertTrue(streets.isEmpty());
+        assertTrue(streets.isEmpty());
     }
 
     @Test
@@ -62,11 +63,72 @@ public class StreetServiceImplTests {
         List<Street> streets = streetService.getStreets();
 
         // Then:
-        Assertions.assertEquals(2, streets.size());
-        Assertions.assertEquals("id1", streets.get(0).getId());
-        Assertions.assertEquals("Street 1", streets.get(0).getName());
-        Assertions.assertEquals("id2", streets.get(1).getId());
-        Assertions.assertEquals("Street 2", streets.get(1).getName());
+        assertEquals(2, streets.size());
+        assertEquals("id1", streets.get(0).getId());
+        assertEquals("Street 1", streets.get(0).getName());
+        assertEquals("id2", streets.get(1).getId());
+        assertEquals("Street 2", streets.get(1).getName());
+    }
+
+
+    @Test
+    public void WhenStreetServiceImplisAdded(){
+        //Given:
+        StreetRepository streetRepository = Mockito.mock(StreetRepository.class);
+        HousesRepository housesRepository = Mockito.mock(HousesRepository.class);
+        RepresentativeRepository representativeRepository = Mockito.mock(RepresentativeRepository.class);
+        StreetServiceImpl info = new StreetServiceImpl(streetRepository,representativeRepository,housesRepository);
+        String streetID = "108412";
+
+
+        Street street = new Street();
+        street.setName("Test Street");
+        Mockito.when(streetRepository.findById(streetID)).thenReturn(Optional.of(street));
+        Representative representative = new Representative();
+        representative.setName("Cristopher");
+        representative.setId("ABC123");
+        representative.setStreet("Street Representative");
+        representative.setPhone("444498751");
+        representative.setAddress("Cerro del faro #1920");
+        Mockito.when(representativeRepository.findRepresentativeByStreet(streetID)).thenReturn(representative);
+        List<House> list1 = new LinkedList<>();
+        House house1 = new House();
+        house1.setId("001");
+        house1.setStreet("Street001");
+        house1.setNumber("004");
+        house1.setChipsEnabled(true);
+        list1.add(house1);
+        House house2 = new House();
+        house2.setId("002");
+        house2.setStreet("Street002");
+        house2.setNumber("009");
+        house2.setChipsEnabled(true);
+        list1.add(house2);
+        House house3 = new House();
+        house3.setId("003");
+        house3.setStreet("Street003");
+        house3.setNumber("003");
+        house3.setChipsEnabled(true);
+        list1.add(house3);
+        Mockito.when(housesRepository.findAllByStreet(streetID)).thenReturn(list1);
+
+        //When:
+        StreetInfo streetInfo = info.getStreetInfo(streetID);
+
+
+        //Then:
+        assertEquals(  "Test Street", streetInfo.getName() );
+        assertEquals("108412",streetInfo.getId());
+        assertEquals("Cristopher",streetInfo.getRepresentative().getName());
+        assertEquals("ABC123", streetInfo.getRepresentative().getId());
+        assertEquals("Street Representative", streetInfo.getRepresentative().getStreet());
+        assertEquals(3,streetInfo.getHouses().size());
+        assertEquals("004", streetInfo.getHouses().get(0).getNumber());
+        assertEquals("009", streetInfo.getHouses().get(1).getNumber());
+        assertEquals("003", streetInfo.getHouses().get(2).getNumber());
+        assertEquals("Street003", streetInfo.getHouses().get(2).getStreet());
+        assertEquals("003", streetInfo.getHouses().get(2).getId());
+
     }
 
 }
